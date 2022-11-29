4d1ca83b590193c2e1d9363541b8fe8877d7b0ac
DougFSilva <doug.ferreira.dfs@gmail.com>
2022-11-29 07:27:51 -0300

    Iniciado o projeto

---
+++ src/test/java/br/com/clinicaEstetica/ClinicaEsteticaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinicaEstetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaEsteticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
